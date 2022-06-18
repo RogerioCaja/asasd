9bf6f56cca766673d043900d8552fa083bc9902a
NVilela <nathan.vilela@nescara.com>
2022-06-18 12:06:31 -0300

    ad

--- force-app/main/default/lwc/orderHeaderScreen/orderHeaderScreen.js
+++ force-app/main/default/lwc/orderHeaderScreen/orderHeaderScreen.js
@@ -46,7 +46,6 @@ export default class OrderHeaderScreen extends LightningElement {
     barterSale = false;
     currentDate;
     dateLimit;
-    dateStartBilling;
     dateLimitBilling;
 
     @api accountData;
@@ -393,7 +392,11 @@ export default class OrderHeaderScreen extends LightningElement {
             if(this.isFilled(event)){
                 var field = event.target.name;
                 if(event.target.value || event.target.checked){
+<<<<<<< HEAD
                     if ((field == 'data_pagamento' || (field == 'data_entrega')) && ((field == 'data_pagamento' && this.currentDate > event.detail.value) || (field == 'data_pagamento' && this.dateLimit < event.detail.value) || (field == 'data_entrega' && this.dateLimitBilling < event.detail.value) || (field == 'data_entrega' && this.dateStartBilling > event.detail.value))) 
+=======
+                    if ((field == 'data_pagamento' || (field == 'data_entrega')) && (this.currentDate > event.detail.value || (field == 'data_pagamento' && this.dateLimit < event.detail.value) || (field == 'data_entrega' && this.dateLimitBilling < event.detail.value))) 
+>>>>>>> parent of ec9237d (changes requirements)
                     {
                         this.headerDictLocale[field] = null;
                         let headerValues = JSON.parse(JSON.stringify(this.headerData));
@@ -472,7 +475,6 @@ export default class OrderHeaderScreen extends LightningElement {
                 const data = JSON.parse(result);
                 this.dateLimit = data.paymentDate;
                 this.dateLimitBilling = data.endDateBilling;
-                this.dateStartBilling = data.startDateBilling;
             })
             .catch((err)=>{
                 console.log(err);
