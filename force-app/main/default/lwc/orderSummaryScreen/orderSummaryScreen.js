9ea96c8725310128e1f88e4b0a9d154b37d41dcd
NVilela <nathan.vilela@nescara.com>
2022-09-08 10:53:58 -0300

    remove condition

--- force-app/main/default/lwc/orderSummaryScreen/orderSummaryScreen.js
+++ force-app/main/default/lwc/orderSummaryScreen/orderSummaryScreen.js
@@ -222,14 +222,11 @@ export default class OrderSummaryScreen extends LightningElement {
     onSelectDistrCenter(){
         this.showLoading = true;
         try{
-            if(this.isFilled(this.summaryDataLocale.centerId)){
-                this.selectDistributionCenter = !this.selectDistributionCenter;
-                this.summaryDataLocale.centerId = this.selectedDistributionCenter.centerId;
-                this.verifyProdDisponiblity();
-            } else {
-                this.showLoading = false;
-            }
-        
+            
+            this.selectDistributionCenter = !this.selectDistributionCenter;
+            this.summaryDataLocale.centerId = this.selectedDistributionCenter.centerId;
+            this.verifyProdDisponiblity();
+            
             const setSummaryData = new CustomEvent('setsummarydata');
             setSummaryData.data = this.summaryDataLocale;
         
