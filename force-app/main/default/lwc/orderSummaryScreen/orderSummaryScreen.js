67060bc023bbe1c719979f51dcbebd4d2439fccd
NVilela <nathan.vilela@nescara.com>
2022-09-08 15:05:21 -0300

    Update orderSummaryScreen.js

--- force-app/main/default/lwc/orderSummaryScreen/orderSummaryScreen.js
+++ force-app/main/default/lwc/orderSummaryScreen/orderSummaryScreen.js
@@ -217,6 +217,7 @@ export default class OrderSummaryScreen extends LightningElement {
     }
 
     openFormOfPayment(event) {
+        this.recalcTotalToDistribution();
         let allPayments = JSON.parse(JSON.stringify(this.formsOfPayment));
         let savedPayments = []
         for (let index = 0; index < allPayments.length; index++) {
@@ -248,7 +249,7 @@ export default class OrderSummaryScreen extends LightningElement {
                     fieldValue = fieldValue.toString().includes(',') ? fieldValue.toString().replace(',', '.') : fieldValue;
                     allPayments[index].value = this.fixDecimalPlaces(fieldValue);
                     allPayments[index].valueFront = this.fixDecimalPlacesFront(fieldValue);
-                    this.recalcTotalToDistribution(allPayments);
+                    this.recalcTotalToDistribution();
                 }
             }
         }
@@ -256,8 +257,9 @@ export default class OrderSummaryScreen extends LightningElement {
         this.formsOfPayment = JSON.parse(JSON.stringify(allPayments));
     }
 
-    recalcTotalToDistribution(allPayments){
+    recalcTotalToDistribution(){
         let value = 0;
+        let allPayments = JSON.parse(JSON.stringify(this.formsOfPayment))
         for (let index = 0; index < allPayments.length; index++) {
             value += allPayments[index].value;
         }
@@ -331,6 +333,7 @@ export default class OrderSummaryScreen extends LightningElement {
         }
 
         this.formsOfPayment = JSON.parse(JSON.stringify(linesToUse));
+        this.recalcTotalToDistribution();
     }
 
     showToast(type, title, message) {
