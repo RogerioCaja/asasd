34d7839aab3f57066cc480255c1a281ddcbcc44d
NVilela <nathan.vilela@nescara.com>
2022-11-22 17:40:45 -0300

    Update orderProductScreen.js

--- force-app/main/default/lwc/orderProductScreen/orderProductScreen.js
+++ force-app/main/default/lwc/orderProductScreen/orderProductScreen.js
@@ -155,18 +155,23 @@ export default class OrderProductScreen extends LightningElement {
 
         if(this.headerData.IsOrderChild) {
             this.disableSearch = true;
-            console.log(JSON.stringify(this.headerData.Id))
-            if (this.seedSale) {
-                let value = this.allProductsBrokerageMother.find(element => element.productId == this.addProduct.productId);
-                this.addProduct.brokerage =  this.isFilled(value) ? this.addProduct.quantity * Number(value.brokeragePerUnit) : this.addProduct.brokerage;
-                this.addProduct.brokerageFront = this.fixDecimalPlacesFront(this.addProduct.brokerage);
-                this.addProduct.totalPriceWithBrokerage = Number(this.addProduct.totalPrice) + Number(this.addProduct.brokerage);
-                this.addProduct.totalPriceWithBrokerageFront = this.fixDecimalPlacesFront(this.addProduct.totalPriceWithBrokerage);
-                this.addProduct.tsiTotalPrice = this.addProduct.tListPrice * this.addProduct.quantity;
-                this.addProduct.tsiTotalPriceFront = this.fixDecimalPlacesFront(this.addProduct.tsiTotalPrice);
-                this.addProduct.royaltyTotalPrice = this.addProduct.rListPrice * this.addProduct.quantity;
-                this.addProduct.royaltyTotalPriceFront = this.fixDecimalPlacesFront(this.addProduct.royaltyTotalPrice);
-            }
+            getBrokerageQuantities({orderId : this.headerData.Id})
+            .then((result) => {
+                if(result){
+                    this.allProductsBrokerageMother = JSON.parse(result);
+                    let brokProducts = [];
+                    for(let i = 0; i < this.products.length; i++){
+                        let productId = this.products[i].productId;
+                        let value = this.allProductsBrokerageMother.find(element => element.productId == productId);
+                        this.products[i].brokerage =  this.isFilled(value) ? this.products[i].quantity * Number(value.brokeragePerUnit) : this.products[i].brokerage;
+                        this.products[i].brokerageFront =  this.fixDecimalPlacesFront(this.products[i].brokerage);
+                        this.products[i].totalPriceWithBrokerage = this.products[i].totalPrice + this.products[i].brokerage;
+                        this.products[i].totalPriceWithBrokerageFront = this.fixDecimalPlacesFront(this.products[i].totalPriceWithBrokerage);
+                        brokProducts.push(this.products[i]);
+                    }
+                    this.products = JSON.parse(JSON.stringify(brokProducts));
+                }
+            })
             this._setData();
         }
 
