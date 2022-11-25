349d17d3adfd367f97b13a0d20ae92b4a251b6bf
Sergio-Neto <52018060+Sergio-Neto@users.noreply.github.com>
2022-11-25 10:31:49 -0300

    Correct item positions

--- force-app/main/default/lwc/orderProductScreen/orderProductScreen.js
+++ force-app/main/default/lwc/orderProductScreen/orderProductScreen.js
@@ -91,7 +91,6 @@ export default class OrderProductScreen extends LightningElement {
 
     disabled=false;
     disableSearch=false;
-    unitPriceDisabled=false;
     numberOfRowsToSkip=0;
     showLoading=true;
     combosIds = [];
@@ -175,7 +174,6 @@ export default class OrderProductScreen extends LightningElement {
             this.headerData.status_pedido == 'Em aprovação - Diretor' || this.headerData.status_pedido == 'Em aprovação - Comitê Margem' || this.headerData.status_pedido == 'Em aprovação - Mesa de Grãos') {
             this.disabled = true;
             this.disableSearch = true;
-            this.unitPriceDisabled = true;
         }
 
         if (this.isFilled(this.commoditiesData) && this.commoditiesData.length > 0) this.showCommodityData = true;
@@ -321,17 +319,10 @@ export default class OrderProductScreen extends LightningElement {
             comboDiscountValue: this.isFilled(currentProduct.comboDiscountValue) ? currentProduct.comboDiscountValue : 0,
             comboId: this.isFilled(currentProduct.comboId) ? currentProduct.comboId : null,
             industryCombo: this.isFilled(currentProduct.comboId) ? currentProduct.industryCombo : false,
-            containsCombo: this.isFilled(currentProduct.containsCombo) ? currentProduct.containsCombo : false,
+            containsCombo: this.isFilled(currentProduct.comboId) ? true : false,
             formerItem: this.isFilled(currentProduct.formerItem) ? currentProduct.formerItem : false,
             benefitItem: this.isFilled(currentProduct.benefitItem) ? currentProduct.benefitItem : false
         };
-        if (this.isFilled(newProduct.comboId)) {
-            this.disabled = true;
-            this.unitPriceDisabled = true;
-        } else {
-            this.disabled = false;
-        }
-
         return newProduct;
     }
  
@@ -367,6 +358,7 @@ export default class OrderProductScreen extends LightningElement {
                 lastPosition = this.products[index].position;
             }
         }
+        return lastPosition;
     }
 
     onSelectCompany() {
@@ -908,13 +900,6 @@ export default class OrderProductScreen extends LightningElement {
             formerItem: this.isFilled(comboValues) ? comboValues.formerItem : false,
             benefitItem: this.isFilled(comboValues) ? comboValues.benefitItem : false
         };
-
-        if (this.isFilled(currentProduct.comboId)) {
-            this.disabled = true;
-            this.unitPriceDisabled = true;
-        } else {
-            this.disabled = false;
-        }
         return newProductData;
     }
 
@@ -1903,7 +1888,6 @@ export default class OrderProductScreen extends LightningElement {
         if ((this.isFilled(this.comboProducts.formerIds) && this.comboProducts.formerIds.length > 0) ||
             (this.isFilled(this.comboProducts.benefitsIds) && this.comboProducts.benefitsIds.length > 0)) {
             this.checkCombo = true;
-            this.unitPriceDisabled = true;
         }
 
         this.getCompanies(getCompanyData);
