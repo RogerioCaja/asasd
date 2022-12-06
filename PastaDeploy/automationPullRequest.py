1083708773e7129d1f9e9bbe4a8a54b178eac2c5
NVilela <nathan.vilela@nescara.com>
2022-12-06 11:28:39 -0300

    Update automationPullRequest.py

--- PastaDeploy/automationPullRequest.py
+++ PastaDeploy/automationPullRequest.py
@@ -157,18 +157,20 @@ class ProcessAutomation:
 
     def prepare_data_git(self) -> None:
         self.prepare_enviroments("develop", self.branch_merge_develop)
-        self.prepare_enviroments("qa2", self.branch_merge_qa2)
-        self.prepare_enviroments("qa", self.branch_merge_qa)
+        # self.prepare_enviroments("qa2", self.branch_merge_qa2)
+        # self.prepare_enviroments("qa", self.branch_merge_qa)
         self.prepare_pull_request()
     
     def prepare_enviroments(self, env, branch_merge) -> None:
         command_1 = ["git", "checkout", env]
         command_2 = ["git", "checkout","-b", branch_merge, env]
-        command_3 = ["git", "merge", self.branch_safe_name]
+        command_3 = ["git", "switch", branch_merge]
+        command_4 = ["git", "merge", self.branch_safe_name]
         command_push = ["git", "push", "origin", branch_merge]
         run(command_1)
         run(command_2)
         run(command_3)
+        run(command_4)
         run(command_push)
 
     def prepare_pull_request(self) -> None:
@@ -176,17 +178,17 @@ class ProcessAutomation:
         self.dict_payload["title"] = self.branch_merge_develop.capitalize()
         payload = json.dumps(self.dict_payload)
 
-        self.dict_payload["destination"]["branch"]["name"] = "qa2"
-        self.dict_payload["source"]["branch"]["name"] = self.branch_merge_qa2
-        self.dict_payload["title"] = self.branch_merge_qa2.capitalize()
+        # self.dict_payload["destination"]["branch"]["name"] = "qa2"
+        # self.dict_payload["source"]["branch"]["name"] = self.branch_merge_qa2
+        # self.dict_payload["title"] = self.branch_merge_qa2.capitalize()
 
-        payload2 = json.dumps(self.dict_payload)
+        # payload2 = json.dumps(self.dict_payload)
 
-        self.dict_payload["destination"]["branch"]["name"] = "qa"
-        self.dict_payload["source"]["branch"]["name"] = self.branch_merge_qa
-        self.dict_payload["title"] = self.branch_merge_qa.capitalize()
+        # self.dict_payload["destination"]["branch"]["name"] = "qa"
+        # self.dict_payload["source"]["branch"]["name"] = self.branch_merge_qa
+        # self.dict_payload["title"] = self.branch_merge_qa.capitalize()
 
-        payload3 = json.dumps(self.dict_payload)
+        # payload3 = json.dumps(self.dict_payload)
 
         requests.request(
             "POST",
@@ -195,19 +197,19 @@ class ProcessAutomation:
             headers=self.headers
         )
 
-        requests.request(
-            "POST",
-            self.url,
-            data=payload2,
-            headers=self.headers
-        )
+        # requests.request(
+        #     "POST",
+        #     self.url,
+        #     data=payload2,
+        #     headers=self.headers
+        # )
 
-        requests.request(
-            "POST",
-            self.url,
-            data=payload3,
-            headers=self.headers
-        )
+        # requests.request(
+        #     "POST",
+        #     self.url,
+        #     data=payload3,
+        #     headers=self.headers
+        # )
 
 if __name__ == '__main__':
     ProcessAutomation().prepare_data_git()
