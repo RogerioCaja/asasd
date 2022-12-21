b4b4cde810a1f77bc85ffc7e289566fb2e6f7790
Sergio-Neto <sergio.neto@nescara.com>
2022-12-21 16:49:41 -0300

    Add freight to total price

--- force-app/main/default/lwc/orderSummaryScreen/orderSummaryScreen.js
+++ force-app/main/default/lwc/orderSummaryScreen/orderSummaryScreen.js
@@ -316,6 +316,8 @@ export default class OrderSummaryScreen extends LightningElement {
                 this.summaryDataLocale.orderMargin = this.orderMargin;
             }
 
+            this.summaryDataLocale.totalValue = orderTotalPrice + royaltiesTotalPrice + tsiTotalPrice;
+
             this.defineOrderMargin();
         }
     }
